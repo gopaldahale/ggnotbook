ef8d67a9d03ce5b0f43a0d86b4842fb44c8feed9
gopaldahale171 <gdahale@truperformance.us>
2025-01-13 20:02:54 +0530

    ssds

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
-const connectToMongo = require('./db');
+const express = require('express')
 const cors = require('cors');
-// ... rest of your code
+const connectToMongo = require('./db');
 connectToMongo();  // Call the function to connect to MongoDB'
 
-const express = require('express')
 const app = express()
 // const port = 5000
 
-app.use(express.json()); // middleware to parse json
 // app.use(cors());
 app.use(cors({
   origin: 'https://ggnotebook.vercel.app',  
   methods: ["POST", "GET", "DELETE", "PUT", "OPTIONS"],   
   credentials: true,   
 }));
- 
+
+app.use(express.json()); // middleware to parse json
 
 // Available Routes 
 // app.use('/api/auth', require('./routes/auth'))
