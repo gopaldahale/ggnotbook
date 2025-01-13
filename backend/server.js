bc887bb2040e7b2f7ffa281d6077e5e5ec2b16ec
gopaldahale171 <gdahale@truperformance.us>
2025-01-13 22:26:12 +0530

    hola

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,25 @@ const app = express()
 app.use(cors({
   origin: 'https://ggnotebook.vercel.app',  
   methods: ["POST", "GET", "DELETE", "PUT", "OPTIONS"],   
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,   
 }));
 
-app.options('*', cors());
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://ggnotebook.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204);
+});
 
 app.use(express.json()); // middleware to parse json
 
 // Available Routes 
-// app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('./routes/notes'))
-app.use('/api/auth', require('./api/auth'))
-app.use('/api/notes', require('./api/notes'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+// app.use('/api/auth', require('./api/auth'))
+// app.use('/api/notes', require('./api/notes'))
 
 
 
