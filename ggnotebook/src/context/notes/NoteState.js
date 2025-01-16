86a7b9ff28b871f19e3739da2896b3c82dea9dd7
gopaldahale171 <gdahale@truperformance.us>
2025-01-16 20:51:45 +0530

    fds

--- ggnotebook/src/context/notes/NoteState.js
+++ ggnotebook/src/context/notes/NoteState.js
@@ -2,8 +2,8 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000";
-    // const host = "https://ggnotbook-backend.vercel.app";
+    // const host = "http://localhost:5000";
+    const host = "https://ggnotbook-backend.vercel.app";
 
     // Get all notes 
     const [notes, setNotes] = useState([]);
