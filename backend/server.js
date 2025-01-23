8e4eaaf9674c10cdfa8e7136eeb04b6261ea4962
gopaldahale171 <gdahale@truperformance.us>
2025-01-23 19:32:53 +0530

    simplet backend

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,9 @@
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const connectToMongo = require('./db');
-
-// Connect to MongoDB
-connectToMongo();
-
 const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(morgan('dev'));
-
-// CORS Options
-const corsOptions = {
-  origin: ['https://ggnotebook-backend.vercel.app/'],
-  credentials: true, // Correct "Credential" to "credentials"
-};
-app.use(cors(corsOptions));
-
-// Routes
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/notes', require('./routes/notes'));
 
 // Root Route
 app.get('/', (req, res) => {
-  res.send('Welcome to the main API!');
-});
-
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.send('Backend is working!');
 });
 
 // Export the app for Vercel
