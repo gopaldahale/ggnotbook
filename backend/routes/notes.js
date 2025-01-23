92fc31375db3851636f034c83d66b0cf14a8c453
gopaldahale171 <gdahale@truperformance.us>
2025-01-23 23:45:14 +0530

    sdvs

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -12,7 +12,10 @@ router.get('/', (req, res) => {
 router.get('/fetchallnotes', fetchuser, async (req, res) => {   
     try {
         const notes = await Notes.find({ user: req.user.id });
-        res.json(notes);
+        res.json({
+            success: true,
+            data: notes
+        });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal server error");
