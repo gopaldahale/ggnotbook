52ce4842dd9e5da7ef55dda552e58f5f5e0fd77b
gopaldahale171 <gdahale@truperformance.us>
2025-01-23 19:07:48 +0530

    run

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,12 @@ const express = require('express')
 const cors = require('cors');
 const morgan = require('morgan');
 const connectToMongo = require('./db');
-connectToMongo();  // Call the function to connect to MongoDB'
-
-// app.use(cors({
-  //   origin: 'https://ggnotebook.vercel.app',  
-  //   methods: ["POST", "GET", "DELETE", "PUT", "OPTIONS"],   
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  //   credentials: true,   
-  // }));
+connectToMongo();   
+ 
   const app = express()
   const port = 5000 
   
-  app.use(express.json()); // middleware to parse json
+  app.use(express.json());  
   app.use(morgan('dev'));
 
   const corsOptions = {
@@ -24,8 +18,8 @@ connectToMongo();  // Call the function to connect to MongoDB'
   app.use(cors(corsOptions));
   
 // Available Routes 
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes')) 
+// app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes')) 
 
 app.get('/', (req, res) => {
   res.send('Welcome to the main API!');
@@ -36,8 +30,8 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-app.listen(port, () => {
-  console.log(`ggNotebook Cloud Backend listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`ggNotebook Cloud Backend listening on port ${port}`)
+// })
 
 module.exports = app; 
